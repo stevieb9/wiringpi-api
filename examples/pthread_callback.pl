35ade81f8cb2397e5955a6e86f4b1ecef691148e
Steve Bertrand <steveb@cpan.org>
2016-08-19 10:05:53 -0600

    finally got pthread perl working with callback

--- examples/pthread_callback.pl
+++ examples/pthread_callback.pl
@@ -1,13 +1,17 @@
 use warnings;
 use strict;
 
-use Config;
+use Inline (
+    C => 'DATA', 
+    libs => '-lpthread', 
+    CLEAN_AFTER_BUILD => 0
+);
 
-use Inline (C => 'DATA', libs => '-lpthread', CLEAN_AFTER_BUILD => 0);
+my $x = 20;
 
 create_thread('blah');
 
-print "after callback\n";
+print "after callback, $x\n";
 
 sub blah {
     print "perl callback\n";
@@ -25,21 +29,25 @@ PerlInterpreter *saved;
 void *wrapper(void *sub_name_ptr){
     char *sub_name = (char *)sub_name_ptr;
 
-    PERL_SET_CONTEXT(saved);
     printf("threaded ok, sub: %s\n", sub_name);
+    
+    PERL_SET_CONTEXT(saved);
     {
         dSP;
+        ENTER;
+        SAVETMPS;
         PUSHMARK(SP);
         PUTBACK;
 
-        for (;;){
+        int i;
+        for (i=0;i<3;i++){
             if (access("lock", F_OK) != -1){
                 call_pv(sub_name, G_DISCARD|G_NOARGS);
             }
             else {
                 printf("no trigger\n");
             }
-            sleep(3);
+            sleep(2);
         }
         FREETMPS;
         LEAVE;
@@ -47,12 +55,8 @@ void *wrapper(void *sub_name_ptr){
 }
 
 int create_thread(char *subname){   
-    // this is what fixed things...
-
     saved = Perl_get_context();
-
     pthread_t sub_thread;
- 
    
     if(pthread_create(&sub_thread, NULL, wrapper, subname)) {
         fprintf(stderr, "Error creating thread\n");
