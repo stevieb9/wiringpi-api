d8b5cd51a6c547a585fa87a1f55b796689076920
Steve Bertrand <steveb@cpan.org>
2017-01-17 19:22:30 -0700

    fixed pwm test

--- test/pwm.pl
+++ test/pwm.pl
@@ -15,38 +15,43 @@ wiringPiSetupGpio();
 
 pin_mode($p, PWM_OUT);
 
+pwm_write($p, 0);
+
+is get_alt($p), PWM_OUT, "pin is in pwm output mode ok";
+is $adc->percent < 1, 1, "pwm at 0 in ok range";
+
 pwm_write($p, 512);
 
 is get_alt($p), PWM_OUT, "pin is in pwm output mode ok";
-say "UNKNOWN: " . read_pin($p);
 is $adc->percent > 40 && $adc->percent < 60, 1, "pwm at 512 in ok range";
 
 pwm_write($p, 768);
 
 is get_alt($p), PWM_OUT, "pin is in pwm output mode ok";
-say "UNKNOWN: " . read_pin($p);
 is $adc->percent > 70 && $adc->percent < 80, 1, "pwm at 768 in ok range";
 
 pwm_write($p, 1023);
 
 is get_alt($p), PWM_OUT, "pin is in pwm output mode ok";
-say "UNKNOWN: " . read_pin($p);
 is $adc->percent > 98, 1, "pwm at 1023 in ok range";
 
-pwm_range(9);
+pwm_set_range(9);
 
-pwm_write($p, 4);
+pwm_write($p, 5);
 
 is get_alt($p), PWM_OUT, "pin is in pwm output mode ok";
-say "UNKNOWN: " . read_pin($p);
 is $adc->percent > 40 && $adc->percent < 60, 1, "pwm_range(9): pwm at 4 ok";
 
 pinMode($p, INPUT);
 
 is get_alt($p), INPUT, "pin back to INPUT mode ok";
-is read_pin($p), LOW, "pin back to LOW state ok";
-is $adc->volts < 1, "pin is not outputting any signal ok";
 
-pwm_range(1023);
+# sleep hackery
+select undef, undef, undef, 0.3;
+
+is read_pin($p), LOW, "pin back to LOW state ok";
+is $adc->volts < 1, 1, "pin is not outputting any signal ok";
+
+pwm_set_range(1023);
 
 done_testing();
